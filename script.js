a625df334a50e67a048cea7bfbd00ddfabad2e27
kainpets <pawelstepniak22@gmail.com>
2022-03-27 22:05:25 +0200

    added buttons

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const operate = (operator, num1, num2) => {
+  return `${num1} ${operator} ${num2}`
+}; 
+
+
 const add = arr => arr.reduce((a, b) => a + b);
 const subtract = arr => arr.reduce((a, b) => a - b);
 const multiply = arr => arr.reduce((a, b) => a * b);
 const divide = arr => arr.reduce((a, b) => a / b);
+
