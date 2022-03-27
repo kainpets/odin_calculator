c779fef56db48e87a41a579a41c3607d5156fd80
kainpets <pawelstepniak22@gmail.com>
2022-03-27 21:33:45 +0200

    basic math operations added

---
+++ script.js
@@ -0,0 +1,4 @@
+const add = arr => arr.reduce((a, b) => a + b);
+const subtract = arr => arr.reduce((a, b) => a - b);
+const multiply = arr => arr.reduce((a, b) => a * b);
+const divide = arr => arr.reduce((a, b) => a / b);
