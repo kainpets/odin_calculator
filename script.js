3dd17058aa63484ceb26937f37c4d1fff6558003
kainpets <pawelstepniak22@gmail.com>
2022-03-29 19:50:57 +0200

    rework the class so that it accepts input from user

--- script.js
+++ script.js
@@ -1,123 +1,44 @@
-const inputField = document.querySelector("#input-field");
-let operationArray = [];
-let currentNumber = "";
-let operator = "";
-
-const operate = (numsArray, operator) => {
-  let result;
-  if (operator === "add") {
-    result = add(numsArray);
-  } else if (operator === "subtract") {
-    result = subtract(numsArray);
-  } else if (operator === "divide") {
-    result = divide(numsArray);
-  } else if (operator === "multiply") {
-    result = multiply(numsArray);
+class Calculator {
+  constructor(inputField) {
+    this.inputField = inputField;
+    this.clear();
   }
-  inputField.textContent = result;
-  return result;
-}; 
 
-const add = arr => arr.reduce((a, b) => a + b);
-const subtract = arr => arr.reduce((a, b) => a - b);
-const multiply = arr => arr.reduce((a, b) => a * b);
-const divide = arr => arr.reduce((a, b) => a / b);
-
-const equals = document.querySelector("#equals");
-equals.addEventListener("click", () => {
-  operationArray.push(Number.parseInt(currentNumber, 10));
-  console.log(operationArray);
-  currentNumber = "";
-  operate(operationArray, operator);
-  operationArray.push(operate(operationArray, operator));
-  operationArray.splice(0, 2);
-  console.log(operationArray);
-})
-
-const addition = document.querySelector("#add");
-addition.addEventListener("click", () => {
-  operator = "add";
-  if (operationArray.length === 0) {
-    operationArray.push(Number.parseInt(currentNumber, 10));
-  } 
-  inputField.textContent = "+";
-  currentNumber = "";
-})
-const subtraction = document.querySelector("#subtract");
-subtraction.addEventListener("click", () => {
-  operator = "subtract";
-  if (operationArray.length === 0) {
-    operationArray.push(Number.parseInt(currentNumber, 10));
+  clear() {
+    this.inputField = "";
+    this.operation = undefined;
   }
-  inputField.textContent = "-";
-  currentNumber = "";
-})
-const multiplication = document.querySelector("#multiply");
-multiplication.addEventListener("click", () => {
-  operator = "multiply";
-  if (operationArray.length === 0) {
-    operationArray.push(Number.parseInt(currentNumber, 10));
-  }
-  inputField.textContent = "*";
-  currentNumber = "";
-})
-const division = document.querySelector("#divide");
-division.addEventListener("click", () => {
-  operator = "divide";
-  if (operationArray.length === 0) {
-    operationArray.push(Number.parseInt(currentNumber, 10));
-  }
-  inputField.textContent = "/";
-  currentNumber = "";
-})
 
-const one = document.querySelector("#one");
-one.addEventListener("click", () => {
-  currentNumber += 1;
-  inputField.textContent = currentNumber;
-})
-const two = document.querySelector("#two");
-two.addEventListener("click", () => {
-  currentNumber += 2;
-  inputField.textContent = currentNumber;
-})
-const three = document.querySelector("#three");
-three.addEventListener("click", () => {
-  currentNumber += 3;
-  inputField.textContent = currentNumber;
-})
-const four = document.querySelector("#four");
-four.addEventListener("click", () => {
-  currentNumber += 4;
-  inputField.textContent = currentNumber;
-})
-const five = document.querySelector("#five");
-five.addEventListener("click", () => {
-  currentNumber += 5;
-  inputField.textContent = currentNumber;
-})
-const six = document.querySelector("#six");
-six.addEventListener("click", () => {
-  currentNumber += 6;
-  inputField.textContent = currentNumber;
-})
-const seven = document.querySelector("#seven");
-seven.addEventListener("click", () => {
-  currentNumber += 7;
-  inputField.textContent = currentNumber;
-})
-const eight = document.querySelector("#eight");
-eight.addEventListener("click", () => {
-  currentNumber += 8;
-  inputField.textContent = currentNumber;
-})
-const nine = document.querySelector("#nine");
-nine.addEventListener("click", () => {
-  currentNumber += 9;
-  inputField.textContent = currentNumber;
-})
-const zero = document.querySelector("#zero");
-zero.addEventListener("click", () => {
-  currentNumber += 0;
-  inputField.textContent = currentNumber;
+  appendNumber(number) {
+    this.inputField = number;
+
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+    this.inputField.innerText =  this.inputField;
+  }
+}
+
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
+const inputField = document.querySelector("[data-input]");
+
+const calculator = new Calculator(inputField);
+
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  })
 })
