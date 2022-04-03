8d720789933cf4af5b2fcd2ada063ce5e7bb9c43
kainpets <pawelstepniak22@gmail.com>
2022-04-03 22:20:05 +0200

    Display works. Fix the udnefined previousNumber error

--- script.js
+++ script.js
@@ -1,38 +1,97 @@
-const inputField = document.querySelector("#input-field");
+class Calculator {
+  constructor(previousNumberText, currentNumberText) {
+    this.previousNumberText = previousNumberText;
+    this.currentNumberText = currentNumberText;
+    this.clear();
+  }
+
+  clear() {
+    this.currentNumber = "";
+    this.previousNumber = "";
+    this.operation = undefined;
+  }
+
+  delete() {
+    this.currentNumber = this.currentNumber.toString().slice(0, -1);
+  }
+
+  appendNumber(number) {
+    if (number === '.' && this.currentNumber.includes('.')) return
+    this.currentNumber = this.currentNumber.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.currentNumber === '') return
+    if (this.previousNumber !== '') {
+      this.compute()
+    }
+    this.operation = operation;
+    this.previousNumber = this.currentNumber;
+    this.currentNumber = "";
+  }
+
+  compute() {
+    let computation;
+    const previousNum = parseFloat(this.previousNumber);
+    const currentNum = parseFloat(this.currentNumber);
+    switch(this.operation) {
+      case "+":
+        computation = previousNum + currentNum;
+        break;
+      case "-":
+        computation = currentNum - previousNum;
+        break;
+      case "*":
+        computation = currentNum * previousNum;
+        break;
+      case "÷":
+        computation = previousNum / currentNum;
+        break;
+    }
+    this.currentNumber = computation;
+    this.operation = undefined;
+    this.previousNumber = "";
+  }
+
+  updateDisplay() {
+    this.currentNumberText.innerText = this.currentNumber;
+    if (this.operation !== null) {
+      this.previousNumberText.innerText = `${this.previousNumber} ${this.operation}`
+    } else {
+      this.previousNumberText.innerText = "";
+    }
+  }
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operator]");
+const deleteButton = document.querySelector("[data-delete]");
+const previousNumberText = document.querySelector("[data-previous-number-input]");
+const currentNumberText = document.querySelector("[data-current-number-input]");
 const equalsButton = document.querySelector("[data-equals]");
-const clearButton = document.querySelector("[data-clear]");
 
-const evaluateExpression = (expression) => {
-  let arr = [...expression]
-  console.log(arr);
-}
-
-const updateDisplay = (content) => {
-  inputField.innerText += content;
-}
-
-const clearDisplay = () => {
-  inputField.innerText = "";
-}
+const calculator = new Calculator(previousNumberText, currentNumberText);
 
 numberButtons.forEach(button => {
-  button.addEventListener("click", (e) => {
-    updateDisplay(e.target.innerText);
+  button.addEventListener("click" , () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
   })
 })
 
 operationButtons.forEach(button => {
-  button.addEventListener("click", (e) => {
-    updateDisplay(e.target.innerText);
+  button.addEventListener("click" , () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
   })
 })
 
-clearButton.addEventListener("click", () => {
-  clearDisplay();
+equalsButton.addEventListener("click", () => {
+  calculator.compute();
+  calculator.updateDisplay();
 })
 
-equalsButton.addEventListener("click", () => {
-  evaluateExpression(inputField.innerText);
+deleteButton.addEventListener("click", () => {
+  calculator.delete();
+  calculator.updateDisplay();
 })
